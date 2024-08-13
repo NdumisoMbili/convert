f62516e04f9e4aaa5d96e090525ab037cd9b0959
NdumisoMbili <sn.mbili01@gmail.com>
2024-08-13 22:11:47 +0200

    initial commit

---
+++ convert_celsius_to_fahrenheit.py
@@ -0,0 +1,8 @@
+# Let the user enter the temperature in celsius
+temp_in_celsius = float(input("Enter the temperature in Celsius: "))
+
+# Convert Celsius into Fahrenheit
+temp_in_fahrenheit = (temp_in_celsius * 9/5) + 32
+
+# Print the converted temperature
+print(f"The temperature in Fahrenheit is {temp_in_fahrenheit}.")
